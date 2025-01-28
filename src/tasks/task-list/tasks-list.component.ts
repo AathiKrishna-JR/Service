8b65edaa5515426cfdba13754963e9039c084364
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-28 11:34:44 +0530

    statu

--- src/tasks/task-list/tasks-list.component.ts
+++ src/tasks/task-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
@@ -11,9 +11,22 @@ import { TasksService } from '../tasks.service';
   imports: [TaskItemComponent],
 })
 export class TasksListComponent {
-  public taskService = inject(TasksService);
+  public tasksService = inject(TasksService);
   selectedFilter = signal<string>('all');
-  tasks = this.taskService.allTasks;
+  tasks = computed(()=>{
+    switch (this.selectedFilter()){
+      case 'all':
+        return this.tasksService.allTasks()
+      case 'open':
+          return this.tasksService.allTasks().filter(task => task.status === 'OPEN');
+      case 'in-progress':
+          return this.tasksService.allTasks().filter(task => task.status === 'IN_PROGRESS');
+      case 'done':
+        return this.tasksService.allTasks().filter(task => task.status === 'IN_PROGRESS');
+      default : 
+        return this.tasksService.allTasks();
+      }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
