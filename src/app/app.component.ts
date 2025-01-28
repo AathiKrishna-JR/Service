571a4066eb6b0dbb43f80489a1f45cc46d2597cb
AathiKrishna-JR <aathi3612@gmail.com>
2025-01-28 10:04:53 +0530

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-app',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
-})
-export class AppComponent {
+import { TasksComponent } from '../tasks/tasks.component';
 
-}
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [TasksComponent],
+})
+export class AppComponent {}
